d4bc2ed087e4367778d08249f3b6cff1860d08fd
vizn3r <viznertw@gmail.com>
2025-11-28 21:48:37 +0100

    reorganized stuff

--- boot_check.sh
+++ boot_check.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 mkdir -p build
-rm boot2_checked.S
+rm ./boot/boot2_checked.S
 
 cd build
 
-arm-none-eabi-gcc -c ../boot2.S -o boot2_temp.o
+arm-none-eabi-gcc -c ../boot/boot2.S -o boot2_temp.o
 arm-none-eabi-objcopy -O binary boot2_temp.o boot2_raw.bin
 
 dd if=boot2_raw.bin bs=1 count=252 of=boot2_trimmed.bin 2>/dev/null
-../checksum.py -s 0xffffffff boot2_trimmed.bin boot2_checked.S
-
-cp boot2_checked.S ../
+../checksum.py -s 0xffffffff boot2_trimmed.bin ../boot/boot2_checked.S
--- test.c
+++
@@ -1,5 +0,0 @@
-// test.c
-int global_var = 42;
-int uninitialized;
-
-void my_function(void) { global_var++; }
